1388ed2147911f3e24b48f7147c3d70a662b77ca
Ian Eaves <ian.k.eaves@gmail.com>
2018-04-24 19:34:01 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='fancypipes',
+    version='0.1dev',
+    description='Making scikit-learn pipelines even more useful',
+    url='https://github.com/ieaves/better-pipelines',
+    author='Ian Eaves',
+    author_email='ian.k.eaves@gmail.com',
+    packages=['fancypipes'],
+    license='MIT',
+    long_description=open('README.txt').read(),
+    install_requires=[
+          'sklearn', 'pandas', 'numpy', 'scipy',
+    ],
+    zip_safe=False,
+    include_package_data=True,
+)
